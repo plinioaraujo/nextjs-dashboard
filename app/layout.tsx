f3fb9bc3115e61f777e4edb30c7c950b2cd66c22
Plinio Araujo <plinioglad@gmail.com>
2025-01-22 17:13:25 -0300

    Firth Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
     <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
